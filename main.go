408e01968148112971a927b74c6f0b5b8bdaea89
Lance Finfrock <lfinfrock@chef.io>
2020-02-07 13:31:18 -0800

    Gloo Gatway Adding fan out of request body
    The Amazon API Gateway requires the handler to have
    APIGatewayProxyRequest in the handler. When connecting with Gloo we need a
    handler that receives the body of the request only.
    
    Added the fanning out of the request's body to multiple URLs.
    
    Signed-off-by: Lance Finfrock <lfinfrock@chef.io>

--- main.go
+++ main.go
@@ -1,24 +1,61 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"log"
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Automate struct {
+	URL      string
+	APIToken string
+}
+
 // HandleRequest - handles requests
-func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// If no name is provided in the HTTP request body, throw an error
-	log.Printf("Starting Lambda function")
-	response := req.Body + " Weee!!!"
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       response,
-	}, nil
+func HandleRequest(ctx context.Context, message json.RawMessage) (string, error) {
+
+	for _, automate := range getAutomates() {
+		req, err := http.NewRequest("POST", automate.URL, bytes.NewReader(message))
+		if err != nil {
+			fmt.Printf("error sending request %v", err)
+			continue
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("api-token", automate.APIToken)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Printf("error sending request %v", err)
+			continue
+		}
+
+		if resp.StatusCode != 200 {
+			fmt.Printf("non 200 status code %d", resp.StatusCode)
+		}
+	}
+
+	return "chef demo", nil
 }
 
 func main() {
 	lambda.Start(HandleRequest)
+}
+
+// Pull from a data store
+func getAutomates() []Automate {
+	return []Automate{
+		{
+			URL:      "http://requestbin.sjcmmsn.com/123e2vh1",
+			APIToken: "fake-token-1",
+		},
+		{
+			URL:      "http://requestbin.sjcmmsn.com/1o68e1k1",
+			APIToken: "fake-token-2",
+		},
+	}
 }
